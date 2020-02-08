478e6a5e0ed6e1ca287878a0bbdb719f7d0ab445
AfroFlower <anneliesepalmer@gmail.com>
2020-02-08 15:35:14 -0500

    trying to understand my javascript and added a test js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,44 @@
 $(document).ready(function () {
     alert("Welcome To The Game! Press any letter key to get started!");
 
+    // Make variables for the first letter and/or second letter, 
+    // and other inputs/outputs
+    // firstLetter = "";
+    // secondLetter = "";
+    // operator = "";
+    // isOperatorChosen = false;
+    // isSpelled = false;
+
+    // Here we are provided an initial array of letters.
+    // var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
+    // "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "-"];
+
+    for (var i = 0; i < letters.length; i++) {
+
+        var letterKey = $("<inputLetter>");
+
+        letterKey.attr("data-letter", letters[i]);
+
+        letterKey.text(letters[i]);
+
+        $("#inputLetter").append(letterKey);
+
+    };
+
+    var userText = document.getElementById("inputLetter");
+      // Next, we give JavaScript a function to execute when onkeyup event fires.
+    document.onkeyup = function(event){
+      userText.textContent = event.key;
+
+      var theWord = $("<div>");
+
+      letterGuessed.text($(this).attr("data-letter"));
+
+      $("#inputLetter").append(letterGuessed);
+    }; 
+
 });
 
-var userText = document.getElementById("inputLetter");
-// Next, we give JavaScript a function to execute when onkeyup event fires.
-document.onkeyup = function(event) {
-  userText.textContent = event.key;
-  $("#inputLetter").append()
-};
 
 
 
@@ -20,3 +50,16 @@ document.onkeyup = function(event) {
 // 4. style the css 
 // 5. use a function to alert the user to the page when it first loads
 // 6. 
+
+// ***From the calculator test, use this example to clear your game when the 
+// final image result pops up*** Use a function to initialize our calculator.
+      // This way when the user hits clear, we can guarantee a reset of the app.
+      // function initializeCalculator() {
+      //   firstNumber = "";
+      //   secondNumber = "";
+      //   operator = "";
+      //   isOperatorChosen = false;
+      //   isCalculated = false;
+
+      //   $("#first-number, #second-number, #operator, #result").empty();
+      // }
