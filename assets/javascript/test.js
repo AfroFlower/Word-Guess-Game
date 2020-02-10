2a462e588e5a3c2532c288940f1ff26487ad3fbd
AfroFlower <anneliesepalmer@gmail.com>
2020-02-09 20:45:23 -0500

    created word arrys

--- assets/javascript/test.js
+++ assets/javascript/test.js
@@ -1,44 +1,56 @@
 $(document).ready(function (){
     alert("Welcome To The Game! Press any letter key to get started!");
 
-    // WORD Arry VARIABLES
+    // Word Arry Variables
     var words = ["rose", "tulip", "sunflower", "daisy",]
+    // randomizes the word
     var word = words[Math.floor(Math.random() * words.length)];
 
-        var answerArray = [];
-        for (var i = 0; i < word.length; i++) {
-            answerArray[i] = "_";
-        }
-        var remainingLetters = word.length;
-        console.log(remainingLetters)
+    // answerArray inputs underscores in word length value
+    var answerArray = [];
+    for (var i = 0; i < word.length; i++) {
+        answerArray[i] = "_";
+    }
+    
+    var remainingLetters = word.length;
+    console.log(answerArray)
 
     var winningWord = "";
     var remainingGuesses = 0;
     var correctLetters = "";
     var wrongLetters = "";
     var wins = 0;
-    var loss = 0;
+    var loss = 0;    
 
 
-        var userText = document.getElementById("correct-letter");
-            document.onkeyup = function(event){
-            userText.textContent = event.key;
+    var userText = document.getElementById("correct-letter");
+        document.onkeyup = function(event){
+           userText.textContent = event.key;
+            
+           remainingLetters = $("correct-letter");
 
-            correctLetters = $("<correct-letters>");
+            // $("#correct-letter").append(correctLetters);
 
-            // when user presses correct letter, add letter to correct letters that add to input letters
-            if ( correctLetters == words) {
-                correctLetters += $(this).val();
-                $("#correct-letters").append(answerArray);
 
-            // if (remainingLetters > 0){
-            //     // Show the player their progress
-            //     $("#correct-letter").append(answerArray.join(" "));
-            // }        
+            if (remainingLetters > 0) {
+                // Show the player their progress
+                return (answerArray.join(" "));
+            };   
 
-            // console.log(remainingLetters);
+            console.log(remainingLetters)
             
         };
-    };
+
+        // if (isOperatorChosen) {
+        //     secondNumber += $(this).val();
+        //     $("#second-number").text(secondNumber);
+  
+        //   }
+        //   else {
+        //     firstNumber += $(this).val();
+        //     $("#first-number").text(firstNumber);
+        //   }
+  
+        // });
 });
     
